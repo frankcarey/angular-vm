637c2373e5568ef17c9be9dd5c84f59b7bc869dc
Frank Carey <frank@zivtech.com>
2014-05-20 12:13:45 -0400

    Fix the ruby package names in ubuntu 14.04 and include the yeoman package.

--- puppet-manifests/site.pp
+++ puppet-manifests/site.pp
@@ -1,6 +1,19 @@
 
-# Install node.js via https://forge.puppetlabs.com/willdurand/nodejs
-# See that url for installing multiple versions, and more options.
-class { 'nodejs':
-    version => 'stable',
+node default {
+  # Install node.js via https://forge.puppetlabs.com/willdurand/nodejs
+  # See that url for installing multiple versions, and more options.
+  class { 'nodejs':
+      version => 'stable',
+  }
+
+  # Install ruby and ruby gems via https://github.com/puppetlabs/puppetlabs-ruby
+  # This is needed to grab compass, etc.
+  class { 'ruby':
+      # Ubunutu doesn't use the default package names. See issue: https://github.com/puppetlabs/puppetlabs-ruby/pull/24
+      ruby_package => 'ruby1.9.1',
+      rubygems_package => 'ruby1.9.1-full',
+  }
+  # Install our custom yoeman, grunt, etc configuration.
+  include yeoman
+
 }
