2d953ead2f418e4d42c9ea1c81c7b6566a365d76
Frank Carey <frank@zivtech.com>
2014-08-20 19:13:09 -0400

    Use a variable for setting up the projects folder. Create a symlink to the projects folder inside /home/vagrant. Setup proper ordering of steps when building a yeoman project.

--- puppet-manifests/site.pp
+++ puppet-manifests/site.pp
@@ -41,8 +41,22 @@ node default {
       hosts => 'github.com,bitbucket.org',
       user => 'vagrant',
   }
+
+  # CUSTOMIZE: location of projects. Defaults to /vagrant/projects so that it's shared locally, don't use a trailing slash.
+  $projects = '/vagrant/projects'
+
+  # Create the projects directory.
+  file { $projects :
+        ensure => "directory",
+  } ~> #Do first then..
+
+  file { "/home/vagrant/projects":
+      ensure => 'link',
+      target => $projects,
+  } ~> #Do first then..
+
   # CUSTOMIZE: Update the location / name of the angular project.
-  vcsrepo { '/vagrant/angular-project':
+  vcsrepo { "${projects}/angular-project":
       ensure   => present,
       provider => git,
       source => "https://github.com/frankcarey/base-angular-project.git"
@@ -65,25 +79,27 @@ node default {
   #  cwd => '/vagrant/angular/base-angular-project',
   #}
 
+  # @TODO: Pull this stuff out and put into an actual puppet-module (yeoman).
   # CUSTOMIZE: You can add additional projects or change the location/name of the angular-project
   exec { "install yoeman-project-dependencies":
         command => "npm install \
-                    && bower install\
+                    && bower install --config.interactive=false \
                     && grunt build",
-        cwd     => '/vagrant/angular-project',
-        require => [Vcsrepo['/vagrant/angular-project'],Class['nodejs']],
+        cwd     => "${projects}/angular-project",
+        require => [Vcsrepo["${projects}/angular-project"],Class['nodejs']],
         # CUSTOMIZE: Comment out the "creates" line to have it always do a fresh build on vagrant reload
-        creates => "/vagrant/angular-project/node_modules",
-        logoutput   => true,
-        timeout     => 1800,
-    }
+        creates => "${projects}/angular-project/node_modules",
+        logoutput => true,
+        loglevel => 'info',
+        timeout => 1800,
+    } ~> #Do this first...
 
   # CUSTOMIZE: You can disable the automatic running of the server for the angular-project by commenting out the exex.
   # Try to run the grunt server. If it times-out just run the command from within the project.
   # Fix: Needs to be "properly daemonized". See https://github.com/mitchellh/vagrant/issues/1553
   exec { 'nohup grunt serve 0<&- &>/dev/null &':
-        cwd     => '/vagrant/angular-project',
-        require => [Vcsrepo['/vagrant/angular-project'],Class['nodejs']],
+        cwd     => "${projects}/angular-project",
+        require => [Vcsrepo["${projects}/angular-project"],Class['nodejs']],
         user => 'vagrant'
     }
 }
